1a68b9c15d8cdf773ab5cc8f4b7dfad82e874d45
zeman <zemanqwe@gmail.com>
2023-04-07 14:49:43 +0800

    main.py

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-import cv2
 from pioneer_sdk import Pioneer, Camera
+import cv2
+import math
 import numpy as np
 
-def load_coefficients(path):
-    """ Loads camera matrix and distortion coefficients. """
-    # FILE_STORAGE_READ
-    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+angle = float(0)
+number_of_points = 10
+increment = float(360 / number_of_points)
+radius = 0.6
+flight_height = float(1)
 
-    # note we also have to specify the type to retrieve other wise we only get a
-    # FileNode object back instead of a matrix
-    camera_matrix = cv_file.getNode("mtx").mat()
-    dist_matrix = cv_file.getNode("dist").mat()
+command_x = radius * math.cos(math.radians(angle))
+command_y = radius * math.sin(math.radians(angle))
+command_yaw = math.radians(angle)
 
-    cv_file.release()
-    return camera_matrix, dist_matrix
+first_point = True
+last_point_reached = False
 
 if __name__ == '__main__':
-    camera_mtx, camera_dist = load_coefficients('data.yml')
-    size_of_marker = 0.1  # side length in meters
-    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_1000)
-    aruco_parameters = cv2.aruco.DetectorParameters_create()
+    print('start')
+    pioneer_mini = Pioneer()
+    pioneer_mini.arm()
+    pioneer_mini.takeoff()
     camera = Camera()
-    mini = Pioneer()
-    # Go to start point
-    mini.arm()
-    mini.takeoff()
-    mini.go_to_local_point(x=0, y=0, z=1.5, yaw=0)
-    while not mini.point_reached():
-        pass
-    send_manual_speed = False
+    while True:
